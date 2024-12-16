a84348b159c997ca6532fbee12fda7485c15f612
Chris White <cwhit163@vols.utk.edu>
2024-12-16 07:32:40 -0800

    json impl

--- bindings/framework.cpp
+++ bindings/framework.cpp
@@ -16,6 +16,31 @@ void bind_processor_help(py::module &m);
 PYBIND11_MODULE(neuro, m) {
     m.doc() = "Neuromorphic Framework";
 
+	py::class_<nlohmann::json>(m, "json", py::dynamic_attr())
+        .def(py::init<>())
+        .def(py::init<py::object>())
+        .def("to_python", [](nlohmann::json &j){
+            py::object obj = j;
+            return obj;
+        })
+        .def(py::pickle(
+            [](nlohmann::json &j){
+                py::object obj = json_to_py(j);
+                return obj;
+            },
+            [](py::object o){
+                nlohmann::json j = py_to_json(o);
+                return j;
+            }
+        ))
+        .def("__repr__", [](nlohmann::json &j) {
+            return j.dump();
+        })
+        .def("__str__", [](nlohmann::json &j) {
+            return j.dump(4);
+        });
+    py::implicitly_convertible<py::object, nlohmann::json>();
+
 	// Bind Nodes, Edges, & Networks 
 	bind_framework_network(m);
 
